b1097e13987939ebcdd9b47640aee566fc708ecb
Jiyeol Lee <ka.felix496@gmail.com>
2023-02-11 03:00:00 -0500

    finish section 2

--- index.js
+++ index.js
@@ -8,14 +8,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  events.push(event);
+
+  axios.post("http://localhost:4000/events", event).catch((_) => {});
+  axios.post("http://localhost:4001/events", event).catch((_) => {});
+  axios.post("http://localhost:4002/events", event).catch((_) => {});
+  axios.post("http://localhost:4003/events", event).catch((_) => {});
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (_, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
